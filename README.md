70ff036f8545e7c3f93ce05d1f9a931213c8358f
Jeppe <jeppe.holmandersen@gmail.com>
2025-11-22 14:01:04 +0100

    WeatherAPIIntergrated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GithubActionsExperimentRepo
 This repository is to play with github actions
+
+## Schedule
+
+Test the schedule action:
+
+A simple weather app that calls a weather api that gets it at 8 am.
+It then sends an email with the current weather and what kind of 
+clothing to wear.
---
+++ main.py
@@ -0,0 +1,41 @@
+﻿import requests
+from dotenv import find_dotenv, load_dotenv
+import os
+
+def get_posts():
+    env_path = find_dotenv()
+    load_dotenv(env_path)
+    API_KEY = os.getenv("API_KEY")
+    CITY_NAME = "Odense"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            posts = response.json()
+            return posts
+        else:
+            print("Error:", response.status_code)
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return None
+
+
+def main():
+    posts = get_posts()
+    print(posts)
+    print("Weather:")
+    print(posts["weather"][0]["main"])
+    print("Temperature:")
+    print(round(posts["main"]["temp"] - 273))
+    print("Wind speed:")
+    print(posts["wind"]["speed"])
+
+
+
+
+if __name__ == "__main__":
+    main()
